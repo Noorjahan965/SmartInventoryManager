d3757030a29ada7ec6b6f1c12d8cfea5ed54e29c
Sailash05 <sailashs2005@gmail.com>
2026-01-13 12:25:22 +0530

    feat: Initial configuration

---
+++ server/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const app = express();
+
+// app.use(cors({
+//     origin: "http://192.168.1.5:5173", // allow your frontend
+//     credentials: true,               // allow cookies if used
+// }));
+
+app.use(cors({ origin: '*', credentials: true }));  // For development only
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+connectDB();
+
+app.listen(PORT, () => console.log(`🚀 Port running on ${PORT}`))
